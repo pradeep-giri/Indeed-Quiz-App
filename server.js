31134a66160065ceb0c9e1b42c15ce7cd50f2828
unknown <pradeep.g@holorobotics.com>
2019-12-20 21:45:17 +0530

    Deploy to heroku

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static('./dist/mean-project'));
+app.use(express.static('./dist/frontend'));
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/dist/mean-project/index.html'));
+    res.sendFile(path.join(__dirname, '/dist/frontend/index.html'));
 });
 
 app.listen(process.env.PORT || 8080, () => {
